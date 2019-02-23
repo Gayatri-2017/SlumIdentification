1f4628a2982346d6d0f9de1a8792706711f5ccfa
user <gayatrig1997@gmail.com>
2019-02-23 21:47:58 +0300

    Successfully downloaded 10 images but opens the QGIS Software everytime

--- ProgramDatasetDownloadTry/test1.py
+++ ProgramDatasetDownloadTry/test1.py
@@ -10,14 +10,19 @@ y1 = y + ydiff
 loopingvariable=0
 
 import subprocess
-process = subprocess.Popen('ping -c 3 192.168.0.100', shell=True)
-cmd = 'qgis --project GoogleSatellite.qgs --snapshot image.png --width 1500 --height 1000 --extent ' + str(x) + ',' + str(y) + ',' + str(x1) + ',' + str(y1) 
-process = subprocess.Popen(cmd, shell=True)
+#process = subprocess.Popen('ping -c 3 192.168.0.100', shell=True)
+#cmd = 'qgis --project GoogleSatellite.qgs --snapshot image.png --width 1500 --height 1000 --extent ' + str(x) + ',' + str(y) + ',' + str(x1) + ',' + str(y1) 
+#process = subprocess.Popen(cmd, shell=True)
+#process.terminate()
 
 for z in xrange(1,11):
-	print x, y, x1, y1
+	#print x, y, x1, y1
+	cmd = 'qgis --project GoogleSatellite.qgs --snapshot image' + str(z) + '.png --width 200 --height 200 --extent ' + str(x) + ',' + str(y) + ',' + str(x1) + ',' + str(y1) 
+	process = subprocess.Popen(cmd, shell=True)
 	x = x1
 	x1 = x1 + xdiff
 	y = y1
 	y1 = y1 - ydiff
+
+
 	
