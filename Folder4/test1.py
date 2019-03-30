5ef541c972b0c279b8cac0c0358a18bc7ec0e801
user <gayatrig1997@gmail.com>
2019-03-30 22:46:47 +0300

    Images from Google Satellite

--- Folder4/test1.py
+++ Folder4/test1.py
@@ -1,61 +1,16 @@
-'''
-x = 8109284.3
-y = 2164982.0
-
-xdiff = 86.64
-ydiff = -50.16
-
-x1 = x + xdiff
-y1 = y + ydiff
-
-loopingvariable=0
-
-import subprocess
-#process = subprocess.Popen('ping -c 3 192.168.0.100', shell=True)
-#i = 0
-#cmd = 'qgis --project GoogleSatellite.qgs --snapshot image' + str(i) + '.png --width 1500 --height 1000 --extent ' + str(x) + ',' + str(y) + ',' + str(x1) + ',' + str(y1) 
-
-for z in xrange(1,11):
-	print x, y, x1, y1
-	x = x1
-	x1 = x1 + xdiff
-	y = y1
-	y1 = y1 - ydiff
-	cmd = 'qgis --project GoogleSatellite.qgs --snapshot image' + str(i) + '.png --width 1500 --height 1000 --extent ' + str(x) + ',' + str(y) + ',' + str(x1) + ',' + str(y1) 
-	process = subprocess.Popen(cmd, shell=True)
-	
-'''
-
-#x = 8109284.3
-#y = 2164982.0
-#xdiff = 86.64
-#ydiff = -50.16
-
-
-#x,y = 8109710.7,2185828.3
-#x1,y1 = 8110342.0,2185499.8
-
-x, y = 8107572,2177331
-x0, y0 = 8107572,2177331
-x1, y1 = 8108203.3, 2177002.5
+x, y = 8106849,2167219
+x0, y0 = 8106849,2167219
 xdiff,ydiff = 631.3, -328.5
-
-x = x + xdiff
-y = y + ydiff
+x1, y1 = x0+xdiff, y0+ydiff
 
 loopingvariable=0
 
 import subprocess
 import time
-#process = subprocess.Popen('ping -c 3 192.168.0.100', shell=True)
-#cmd = 'qgis --project GoogleSatellite.qgs --snapshot image.png --width 1500 --height 1000 --extent ' + str(x) + ',' + str(y) + ',' + str(x1) + ',' + str(y1) 
-#process = subprocess.Popen(cmd, shell=True)
-#process.terminate()
 
-for z1 in range(1,76):
-	#print x, y, x1, y1
-	for z2 in range(1,3):
-		cmd = 'qgis --project GoogleSatellite.qgs --snapshot image' + str(loopingvariable) + '.png --width 1200 --height 720 --extent ' + str(x) + ',' + str(y) + ',' + str(x1) + ',' + str(y1) 
+for z1 in range(1,77):
+	for z2 in range(1,24):
+		cmd = 'qgis --project GoogleSatellite.qgs --snapshot image' + str(loopingvariable) + '.jpg --width 1200 --height 720 --extent ' + str(x) + ',' + str(y) + ',' + str(x1) + ',' + str(y1) 
 		process = subprocess.Popen(cmd, shell=True)
 		time.sleep(20)
 		x = x1
@@ -65,3 +20,12 @@ for z1 in range(1,76):
 	y1 = y1 + ydiff
 	x = x0
 	x1 = x + xdiff
+
+
+#8121625,2141945
+
+#xrange = 24
+#yrange = 77
+
+#total images = 77 * 24 = 1848
+#total time = 1848 * 20 = 36960 sec = 616 min = 10.3 hrs
